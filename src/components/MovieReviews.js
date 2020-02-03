7a684dced2858dc700d61faca30f3bd2334e668b
Alice Won <alicewon4@gmail.com>
2020-02-02 22:50:35 -0800

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,30 @@
+import React from 'react'
+import { ProgressPlugin } from 'webpack'
 // Code MovieReviews Here
+
+const everyReview = ({
+  headline,
+  byline,
+  link,
+  summary_short
+}) => {
+  return (
+    <div key={headline} className="review">
+
+        <header>
+          <a className="review-link" href={link.url}>{headline}</a>
+          <span className="author">{byline}</span>
+        </header>
+        <blockquote>{summary_short}</blockquote>
+    </div>
+  )
+}
+
+
+const MovieReviews = ({reviews}) => <div className="review-list">{reviews.map(everyReview)}</div>
+
+MovieReviews.defaultProps={
+  reviews: []
+}
+
+export default MovieReviews
